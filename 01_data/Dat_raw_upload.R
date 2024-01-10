c13f736f1dfafe2ef05c611bc02c6daee8c3e893
Mia Grote <mia.grote@charite.de>
2024-01-10 11:18:49 +0100

    edited dat raw upload code

---
+++ 01_data/Dat_raw_upload.R
@@ -0,0 +1,4 @@
+Datenextraktion_komplett <- read_excel("~/Library/Mobile Documents/com~apple~CloudDocs/Documents/2023/CPCOR/Metaanalyse/Datenextraktion - komplett.xlsx", 
+                                       sheet = "HADS_PROMIS included only")
+
+#use for every combination of questionnaires 
