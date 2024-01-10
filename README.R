64c7a68a01b6d4c68a396e81c446747bf03c6c48
Mia Grote <mia.grote@charite.de>
2024-01-10 10:44:19 +0100

    created description of folders

---
+++ README.R
@@ -0,0 +1,21 @@
+# Struktur
+
+#data: Hier werden die Datenquellen gespeichert. Der Ordner raw enthält die Originaldaten, während processed die bereinigten oder verarbeiteten Daten enthält.
+
+#scripts: Dieser Ordner enthält Skripte für verschiedene Schritte der Datenverarbeitung und Analyse.
+
+#notebooks: Hier können Jupyter Notebooks oder R Markdown Dateien für explorative Analysen, Berichte oder Visualisierungen gespeichert werden.
+
+#results: In diesem Ordner werden Ergebnisse der Analyse, erstellte Plots, Modelle oder andere Ausgabedateien gespeichert.
+
+#docs: Dokumentation für das Projekt, einschließlich README-Dateien, Dokumentationsdateien und Anleitungen.
+
+#src: Benutzerdefinierte Funktionen, Hilfsfunktionen oder Module, die in den Skripten oder Notebooks verwendet werden.
+
+#.gitignore: Dateien und Ordner, die von Git ignoriert werden sollen, z. B. temporäre Dateien, Ausgabeordner usw.
+
+#LICENSE: Lizenzinformationen für das Projekt.
+
+#requirements.txt oder environment.yml: Abhängigkeiten und Umgebungskonfigurationen (kann je nach der verwendeten Programmiersprache variieren).
+
+#MetaanalysisCommon Metrics.Rproj: RStudio-Projektdatei 
