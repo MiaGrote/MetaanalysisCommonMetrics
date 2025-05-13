84f2f23d3088fe370e328ab01cf6e4da0ec13735
MiaGrote <mia.grote@online.de>
2025-05-13 17:39:57 +0200

    Update README.R

--- README.R
+++ README.R
@@ -1,13 +1,2 @@
-#Ordnerstruktur 
-Die Ordnerstruktur für Datenanalyse in einem GitHub-Repository kann je nach den spezifischen Anforderungen des Projekts variieren. Es gibt jedoch einige bewährte Praktiken und allgemeine Empfehlungen, die du berücksichtigen kannst. Hier ist eine mögliche Ordnerstruktur für ein Datenanalyseprojekt:
+Man muss immer erst 01_data_cleaning durchlaufen lassen und danach funktionieren die Plots und Analysen Skripts. 
 
-data: Hier werden die Datenquellen gespeichert. Der Ordner raw enthält die Originaldaten, während processed die bereinigten oder verarbeiteten Daten enthält.
-scripts: Dieser Ordner enthält Skripte für verschiedene Schritte der Datenverarbeitung und Analyse.
-notebooks: Hier können Jupyter Notebooks oder R Markdown Dateien für explorative Analysen, Berichte oder Visualisierungen gespeichert werden.
-results: In diesem Ordner werden Ergebnisse der Analyse, erstellte Plots, Modelle oder andere Ausgabedateien gespeichert.
-docs: Dokumentation für das Projekt, einschließlich README-Dateien, Dokumentationsdateien und Anleitungen.
-src: Benutzerdefinierte Funktionen, Hilfsfunktionen oder Module, die in den Skripten oder Notebooks verwendet werden.
-.gitignore: Dateien und Ordner, die von Git ignoriert werden sollen, z. B. temporäre Dateien, Ausgabeordner usw.
-LICENSE: Lizenzinformationen für das Projekt.
-requirements.txt oder environment.yml: Abhängigkeiten und Umgebungskonfigurationen (kann je nach der verwendeten Programmiersprache variieren).
-your_project.Rproj: RStudio-Projektdatei (falls das Projekt in RStudio erstellt wurde).
